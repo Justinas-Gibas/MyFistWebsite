ef24931c729d9d9720d2bcd710c0bc27da01a2c5
JustGibas <116616480+JustGibas@users.noreply.github.com>
2023-05-04 16:16:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const rotationSlider = document.querySelector('#rotation-slider');
-const box = document.querySelector('a-box');
 
-rotationSlider.addEventListener('input', function() {
-  box.setAttribute('rotation', `0 ${rotationSlider.value} 0`);
-});
