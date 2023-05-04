7ff29fb410b194e2f720d4f1cbbf3c726601fecc
JustGibas <116616480+JustGibas@users.noreply.github.com>
2023-05-04 22:17:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const camera = document.querySelector("#mainCamera");
   const blackPlane = document.querySelector("#blackPlane");
   const whitePlane = document.querySelector("#whitePlane");
-  const clickSound = document.querySelector("#click-sound");
+  //const clickSound = document.querySelector("#click-sound");
 
-  function playSound() {
-    if (clickSound) {
-      clickSound.currentTime = 0;
-      clickSound.play();
-    }
-  }
+ // function playSound() {
+   // if (clickSound) {
+     // clickSound.currentTime = 0;
+     // clickSound.play();
+  //  }
+//  }
 
   function handlePlaneClick(event) {
     const clickedPlane = event.target;
 
     if (clickedPlane.id === "blackPlane") {
       localStorage.setItem("userPosition", JSON.stringify(camera.getAttribute("position")));
-      localStorage.setItem("userRotation", JSON.stringify(camera.getAttribute("rotation")));
+     // localStorage.setItem("userRotation", JSON.stringify(camera.getAttribute("rotation")));
       localStorage.setItem("returningFromOutside", "true");
-	PlaySound();
+//	PlaySound();
       window.location.href = "index.html";
     } else if (clickedPlane.id === "whitePlane") {
       localStorage.setItem("userPosition", JSON.stringify(camera.getAttribute("position")));
-      localStorage.setItem("userRotation", JSON.stringify(camera.getAttribute"rotation")));
+  //    localStorage.setItem("userRotation", JSON.stringify(camera.getAttribute"rotation")));
       localStorage.setItem("returningFromOutside", "true");
-	PlaySoud();
+//	PlaySoud();
       window.location.href = "index.html";
     }
   }
@@ -35,14 +35,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const returningFromOutside = localStorage.getItem("returningFromOutside");
   if (returningFromOutside === "true") {
     const userPosition = JSON.parse(localStorage.getItem("userPosition"));
-    const userRotation = JSON.parse(localStorage.getItem("userRotation"));
+//    const userRotation = JSON.parse(localStorage.getItem("userRotation"));
     camera.setAttribute("position", userPosition);
-    camera.setAttribute("rotation", userRotation);
+  //  camera.setAttribute("rotation", userRotation);
 
     // Clear the localStorage flags
     localStorage.removeItem("returningFromOutside");
     localStorage.removeItem("userPosition");
-    localStorage.removeItem("userRotation");
+  //  localStorage.removeItem("userRotation");
 }
 
 });
