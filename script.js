d3cf12b8562538ba549567d3638f57091012f612
JustGibas <justas.gibas@gmail.com>
2023-05-06 01:00:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,9 @@ document.addEventListener("DOMContentLoaded", function () {
   function generateWorld(position) {
     const { x, z } = getChunkCoords(position);
 
+    // Update chunk-info element content
+    chunkInfo.innerHTML = `Current Chunk: x: ${x}, z: ${z}`;
+
     for (let i = -worldRadius; i <= worldRadius; i++) {
       for (let j = -worldRadius; j <= worldRadius; j++) {
         const xPos = x + i * chunkSize;
