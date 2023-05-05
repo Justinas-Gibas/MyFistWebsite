e9b4fb62e6d823a1753ce5349c27c2180d88fbde
JustGibas <justas.gibas@gmail.com>
2023-05-05 17:18:21 +0300

    nauja diena

--- script.js
+++ script.js
@@ -1,58 +1,33 @@
-AFRAME.registerComponent("chunk-manager", {
-  init: function () {
-    this.el.sceneEl.addEventListener("loaded", () => {
-      this.manageChunks();
-    });
-    this.el.sceneEl.addEventListener("componentchanged", (evt) => {
-      if (evt.detail.name === "position") {
-        this.manageChunks();
-      }
-    });
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const world = document.querySelector("#world");
+  const gltfModels = [
+    "#gltfModel1",
+    "#gltfModel2",
+    // Add more model references as needed
+  ];
 
-  manageChunks: function () {
-    const rig = document.querySelector("#rig");
-    const chunks = document.querySelector("#chunks");
-    const rigPosition = rig.getAttribute("position");
+  function createChunk(x, y, z, modelIndex) {
+    const chunk = document.createElement("a-entity");
+    chunk.setAttribute("gltf-model", gltfModels[modelIndex]);
+    chunk.setAttribute("position", { x, y, z });
+    world.appendChild(chunk);
+  }
 
-    // Calculate the chunk the user is in (you can customize the size of the chunks)
+  function generateWorld() {
     const chunkSize = 10;
-    const currentChunk = {
-      x: Math.floor(rigPosition.x / chunkSize),
-      z: Math.floor(rigPosition.z / chunkSize),
-    };
+    const numChunks = 5;
 
-    // Loop through all the chunks and load or unload them based on their distance to the current chunk
-    for (let x = -1; x <= 1; x++) {
-      for (let z = -1; z <= 1; z++) {
-        const chunkId = `chunk-${currentChunk.x + x}-${currentChunk.z + z}`;
+    for (let i = 0; i < numChunks; i++) {
+      for (let j = 0; j < numChunks; j++) {
+        const x = i * chunkSize;
+        const y = 0;
+        const z = j * chunkSize;
+        const modelIndex = Math.floor(Math.random() * gltfModels.length);
 
-        if (!chunks.querySelector(`#${chunkId}`)) {
-          const modelId = `chunk${Math.abs(currentChunk.x + x) % 2 + 1}`; // Replace with your own logic to determine the glTF model to use
-          const chunk = document.createElement("a-entity");
-          chunk.setAttribute("id", chunkId);
-          chunk.setAttribute("gltf-model", `#${modelId}`);
-          chunk.setAttribute("position", {
-            x: (currentChunk.x + x) * chunkSize,
-            y: 0,
-            z: (currentChunk.z + z) * chunkSize,
-          });
-          chunks.appendChild(chunk);
-        }
+        createChunk(x, y, z, modelIndex);
       }
     }
+  }
 
-    // Unload the chunks that are not adjacent
-    chunks.querySelectorAll("a-entity").forEach((chunk) => {
-      const [_, x, z] = chunk.id.split("-").map((n) => parseInt(n));
-      if (
-        x < currentChunk.x - 1 ||
-        x > currentChunk.x + 1 ||
-        z < currentChunk.z - 1 ||
-        z > currentChunk.z + 1
-      ) {
-        chunks.removeChild(chunk);
-      }
-    });
-  },
+  generateWorld();
 });
