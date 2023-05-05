19f227ad0db61f7029cfca6d803fe3110d516f73
JustGibas <justas.gibas@gmail.com>
2023-05-06 01:26:39 +0300

    naujas test

--- script.js
+++ script.js
@@ -1,88 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const scene = document.querySelector("a-scene");
-  const world = document.querySelector("#world");
-  const camera = document.querySelector("a-camera");
-  const assets = document.querySelector("a-assets");
-  const chunkInfo = document.querySelector("#chunk-info");
-  const loadGltfButton = document.querySelector("#loadGltf");
-
-  AFRAME.registerComponent("tick-listener", {
-    tick: function () {
-      onCameraTick();
-    },
-  });
-
-  const gltfModels = [
-    "#gltfModel1",
-    "#gltfModel2",
-  ];
-
-  const chunkSize = 100;
-  const worldRadius = 2;
-  const loadedChunks = new Set();
-
-  function getChunkCoords(position) {
-    const x = Math.floor(position.x / chunkSize) * chunkSize;
-    const z = Math.floor(position.z / chunkSize) * chunkSize;
-    return { x, z };
+AFRAME.registerComponent('update-position', {
+  tick: function () {
+    var position = document.querySelector('#position').getAttribute('position');
+    document.querySelector('#x').innerHTML = position.x;
+    document.querySelector('#y').innerHTML = position.y;
+    document.querySelector('#z').innerHTML = position.z;
   }
+});
 
-  function createChunk(x, z, modelIndex) {
-    const chunk = document.createElement("a-entity");
-    chunk.setAttribute("gltf-model", gltfModels[modelIndex]);
-    chunk.setAttribute("position", { x, y: 0, z });
-    chunk.setAttribute("id", `chunk-${x}-${z}`);
-    world.appendChild(chunk);
-    loadedChunks.add(`chunk-${x}-${z}`);
-  }
-
-  function generateWorld(position) {
-    const { x, z } = getChunkCoords(position);
-
-    // Update chunk-info element content.
-    chunkInfo.innerHTML = `Current Chunk: x: ${x}, z: ${z}`;
-
-    for (let i = -worldRadius; i <= worldRadius; i++) {
-      for (let j = -worldRadius; j <= worldRadius; j++) {
-        const xPos = x + i * chunkSize;
-        const zPos = z + j * chunkSize;
-        const chunkId = `chunk-${xPos}-${zPos}`;
-
-        if (!loadedChunks.has(chunkId)) {
-          const modelIndex = Math.floor(Math.random() * gltfModels.length);
-          createChunk(xPos, zPos, modelIndex);
-        }
-      }
-    }
-  }
-
-  let prevCameraPosition = null;
-
-function onCameraTick() {
-  const currentPosition = camera.getAttribute("position");
-  const currentChunkCoords = getChunkCoords(currentPosition);
-  const prevChunkCoords = prevCameraPosition ? getChunkCoords(prevCameraPosition) : null;
-
-  if (
-    !prevChunkCoords ||
-    currentChunkCoords.x !== prevChunkCoords.x ||
-    currentChunkCoords.z !== prevChunkCoords.z
-  ) {
-    generateWorld(currentPosition);
-    prevCameraPosition = currentPosition;
-
-    // Update chunk-info element content
-    chunkInfo.innerHTML = `Current Chunk: x: ${currentChunkCoords.x}, z: ${currentChunkCoords.z}`;
-  }
-}
-
-  assets.addEventListener("loaded", function () {
-    camera.setAttribute("tick-listener", "");
-    const currentPosition = camera.getAttribute("position");
-    generateWorld(currentPosition); // Call generateWorld initially
-    loadGltfButton.addEventListener("click", function () {
-      const currentPosition = camera.getAttribute("position");
-      generateWorld(currentPosition);
-    });
-  });
+document.querySelector('a-camera').addEventListener('componentinitialized', function (e) {
+  if (e.detail.name !== 'position') { return; }
+  e.target.setAttribute('update-position', '');
 });
