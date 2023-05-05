e7b144572f0873c3449a292b2a68806c3a445452
JustGibas <justas.gibas@gmail.com>
2023-05-05 19:22:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
   const world = document.querySelector("#world");
+  const camera = document.querySelector("a-camera");
   const gltfModels = [
     "#gltfModel1",
     "#gltfModel2",
     // Add more model references as needed
   ];
 
-  function createChunk(x, y, z, modelIndex) {
-    const chunk = document.createElement("a-entity");
-    chunk.setAttribute("gltf-model", gltfModels[modelIndex]);
-    chunk.setAttribute("position", { x, y, z });
-    world.appendChild(chunk);
+  const chunkSize = 10;
+  const numChunks = 5;
+  const worldRadius = 2;
+  const loadedChunks = new Set();
+
+  function getChunkCoords(position) {
+    const x = Math.floor(position.x / chunkSize) * chunkSize;
+    const z = Math.floor(position.z / chunkSize) * chunkSize;
+    return { x, z };
   }
 
-  function generateWorld() {
-    const chunkSize = 100;
-    const numChunks = 2;
+  function createChunk(x, z, modelIndex) {
+    const chunk = document.createElement("a-entity");
+    chunk.setAttribute("gltf-model", gltfModels[modelIndex]);
+    chunk.setAttribute("position", { x, y: 0, z });
+    chunk.setAttribute("id", `chunk-${x}-${z}`);
+    world.appendChild(chunk);
+    loadedChunks.add(`chunk-${x}-${z}`);
+  }
 
-    for (let i = 0; i < numChunks; i++) {
-      for (let j = 0; j < numChunks; j++) {
-        const x = i * chunkSize;
-        const y = 0;
-        const z = j * chunkSize;
-        const modelIndex = Math.floor(Math.random() * gltfModels.length);
+  function generateWorld(position) {
+    const { x, z } = getChunkCoords(position);
 
-        createChunk(x, y, z, modelIndex);
+    for (let i = -worldRadius; i <= worldRadius; i++) {
+      for (let j = -worldRadius; j <= worldRadius; j++) {
+        const xPos = x + i * chunkSize;
+        const zPos = z + j * chunkSize;
+        const chunkId = `chunk-${xPos}-${zPos}`;
+
+        if (!loadedChunks.has(chunkId)) {
+          const modelIndex = Math.floor(Math.random() * gltfModels.length);
+          createChunk(xPos, zPos, modelIndex);
+        }
       }
     }
   }
 
-  generateWorld();
+  camera.addEventListener("componentchanged", function (evt) {
+    if (evt.detail.name === "position") {
+      generateWorld(evt.detail.newData);
+    }
+  });
 });
