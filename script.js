f0b66b10145da9bb8b85cdf5194f3abc8b1f957c
JustGibas <justas.gibas@gmail.com>
2023-05-05 20:54:10 +0300

    darom

--- script.js
+++ script.js
@@ -5,50 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const gltfModels = [
     "#gltfModel1",
     "#gltfModel2",
-    // Add more model references as needed
-  ];
-
-  const chunkSize = 10;
-  const numChunks = 5;
-  const worldRadius = 2;
-  const loadedChunks = new Set();
-
-  function getChunkCoords(position) {
-    const x = Math.floor(position.x / chunkSize) * chunkSize;
-    const z = Math.floor(position.z / chunkSize) * chunkSize;
-    return { x, z };
-  }
-
-  function createChunk(x, z, modelIndex) {
-    const chunk = document.createElement("a-entity");
-    chunk.setAttribute("gltf-model", gltfModels[modelIndex]);
-    chunk.setAttribute("position", { x, y: 0, z });
-    chunk.setAttribute("id", `chunk-${x}-${z}`);
-    world.appendChild(chunk);
-    loadedChunks.add(`chunk-${x}-${z}`);
-  }
-
-  function generateWorld(position) {
-    const { x, z } = getChunkCoords(position);
-
-    for (let i = -worldRadius; i <= worldRadius; i++) {
-      for (let j = -worldRadius; j <= worldRadius; j++) {
-        const xPos = x + i * chunkSize;
-        const zPos = z + j * chunkSize;
-        const chunkId = `chunk-${xPos}-${zPos}`;
-
-        if (!loadedChunks.has(chunkId)) {
-          const modelIndex = Math.floor(Math.random() * gltfModels.length);
-          createChunk(xPos, zPos, modelIndex);
-        }
-      }
-    }adocument.addEventListener("DOMContentLoaded", function () {
-  const world = document.querySelector("#world");
-  const camera = document.querySelector("a-camera");
-  const assets = document.querySelector("a-assets");
-  const gltfModels = [
-    "#gltfModel1",
-    "#gltfModel2",
   ];
 
   const chunkSize = 10;
@@ -85,27 +41,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       }
     }
-  }
-
-  let prevCameraPosition = { x: 0, y: 0, z: 0 };
-
-  function onCameraTick() {
-    const currentPosition = camera.getAttribute("position");
-
-    if (
-      Math.floor(currentPosition.x / chunkSize) !== Math.floor(prevCameraPosition.x / chunkSize) ||
-      Math.floor(currentPosition.z / chunkSize) !== Math.floor(prevCameraPosition.z / chunkSize)
-    ) {
-      generateWorld(currentPosition);
-      prevCameraPosition = currentPosition;
-    }
-  }
-
-  assets.addEventListener("loaded", function () {
-    camera.addEventListener("tick", onCameraTick);
-  });
-});
-
   }
 
   let prevCameraPosition = { x: 0, y: 0, z: 0 };
