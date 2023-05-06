b3365ff06c8d5f172781152f911800c9d6bdf5e8
JustGibas <justas.gibas@gmail.com>
2023-05-06 18:51:36 +0300

    dalykai

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var menuIcon = document.querySelector(".menu-icon");
+  var mainMenu = document.querySelector(".main-menu");
+
+  menuIcon.addEventListener("click", function () {
+    mainMenu.classList.toggle("menu-open");
+  });
+});
+
+
 function showAlert() {
     alert('Hello, World!');
 }
