3bace271e150c9db1698ff6521020d429438763c
JustGibas <justas.gibas@gmail.com>
2023-05-06 09:39:58 +0300

    vel

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-AFRAME.registerComponent('update-position', {
-  tick: function () {
-    var position = document.querySelector('#position').getAttribute('position');
-    document.querySelector('#x').innerHTML = position.x.toFixed(2);
-    document.querySelector('#y').innerHTML = position.y.toFixed(2);
-    document.querySelector('#z').innerHTML = position.z.toFixed(2);
-  }
-});
-
-document.addEventListener('loaded', function () {
-  var camera = document.querySelector('a-camera');
-  camera.addEventListener('componentinitialized', function (e) {
-    if (e.detail.name !== 'position') { return; }
-    e.target.setAttribute('update-position', '');
-  });
-});
+function showAlert() {
+    alert('Hello, World!');
+}
