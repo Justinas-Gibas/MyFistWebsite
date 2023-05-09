0b502dd6738b263152d116a3e0ef2436e6fac685
JustGibas <justas.gibas@gmail.com>
2023-05-09 04:21:32 +0300

    note? git push

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-  var menuIcon = document.querySelector(".menu-icon");
-  var mainMenu = document.querySelector(".main-menu");
+// This is a comment in JavaScript
+// Comments are ignored by the computer and used to explain the code
 
-  menuIcon.addEventListener("click", function () {
-    mainMenu.classList.toggle("menu-open");
-  });
-});
+/* 
+    This is also a comment in JavaScript, but it can span 
+    multiple lines if needed
 */
 
-function showAlert() {
-    alert('Hello, World!');
+// Let's declare a variable named "message" and give it a value
+let message = "Hello, world!";
+
+// We can print the value of our variable to the console
+console.log(message);
+
+// Let's create a function that takes two numbers as arguments and returns their sum
+function addNumbers(num1, num2) {
+  return num1 + num2;
 }
+
+// Now we can call our function and log the result to the console
+let result = addNumbers(5, 3);
+console.log(result);
