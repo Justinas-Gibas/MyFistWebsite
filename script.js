7cfce39dc14e2ba3799d0c903f10273c8f70ae47
JustGibas <justas.gibas@gmail.com>
2023-05-09 08:34:19 +0300

    Scroll smooth

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Select all links with the .nav-link class
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Add a click event listener to each link
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    // Prevent the default link behavior
+    e.preventDefault();
+
+    // Get the target section from the link href attribute
+    const targetSection = document.querySelector(link.getAttribute('href'));
+
+    // Scroll smoothly to the target section
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
 // Create a new Three.js scene
 var scene = new THREE.Scene();
 
