741780710f2e51e27c0f9d283d3818b968a5716c
JustGibas <justas.gibas@gmail.com>
2023-05-09 06:46:35 +0300

    3d cube

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-// This is a comment in JavaScript
-// Comments are ignored by the computer and used to explain the code
+// Create a new Three.js scene
+var scene = new THREE.Scene();
 
-/* 
-    This is also a comment in JavaScript, but it can span 
-    multiple lines if needed
-*/
+// Create a new camera and position it
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
-// Let's declare a variable named "message" and give it a value
-let message = "Hello, world!";
+// Create a new renderer and add it to the page
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// We can print the value of our variable to the console
-console.log(message);
+// Create a new cube and add it to the scene
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-// Let's create a function that takes two numbers as arguments and returns their sum
-function addNumbers(num1, num2) {
-  return num1 + num2;
+// Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
-
-// Now we can call our function and log the result to the console
-let result = addNumbers(5, 3);
-console.log(result);
+animate();
