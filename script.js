16640ac1aa82d96042eb5d1362c7aed8287708d3
JustGibas <justas.gibas@gmail.com>
2023-05-12 22:11:21 +0300

    up js

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-// Select all links with the .nav-link class
-const navLinks = document.querySelectorAll('.nav-link');
+document.addEventListener('DOMContentLoaded', function() {
+  // Select all links with the .nav-link class
+  const navLinks = document.querySelectorAll('.nav-link');
 
-// Add a click event listener to each link
-navLinks.forEach(link => {
-  link.addEventListener('click', (e) => {
-    // Prevent the default link behavior
-    e.preventDefault();
+  // Add a click event listener to each link
+  navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      // Prevent the default link behavior
+      e.preventDefault();
 
-    // Get the target section from the link href attribute
-    const targetSection = document.querySelector(link.getAttribute('href'));
+      // Get the target section from the link href attribute
+      const targetSection = document.querySelector(link.getAttribute('href'));
 
-    // Scroll smoothly to the target section
-    targetSection.scrollIntoView({
-      behavior: 'smooth'
+      // Scroll smoothly to the target section
+      targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
     });
   });
-});
 
-const wrapper = document.querySelector('.wrapper');
+  const wrapper = document.querySelector('.wrapper');
+  let scrollPosition;
 
-window.addEventListener('scroll', () => {
-  const scrollPosition = window.pageYOffset;
-  wrapper.style.transform = `translateY(${scrollPosition * 0.5}px)`;
+  window.addEventListener('scroll', () => {
+    scrollPosition = window.pageYOffset;
+    wrapper.style.transform = `translateY(${scrollPosition * 0.5}px)`;
+  });
 });
