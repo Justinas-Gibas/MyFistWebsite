1f687d012753d3810bf922e5a45a21329442602b
JustGibas <justas.gibas@gmail.com>
2023-05-12 21:32:40 +0300

    Update. remove three

--- script.js
+++ script.js
@@ -23,32 +23,3 @@ window.addEventListener('scroll', () => {
   const scrollPosition = window.pageYOffset;
   wrapper.style.transform = `translateY(${scrollPosition * 0.5}px)`;
 });
-
-// Import the GLTFLoader from the three.js library
-import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader';
-
-// Create a new Three.js scene
-var scene = new THREE.Scene();
-
-// Create a new camera and position it
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Create a new renderer and add it to the page
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Load the .gltf file using the GLTFLoader
-var loader = new GLTFLoader();
-loader.load('.models.chunk1.gltf', function(gltf) {
-  // Add the loaded model to the scene
-  scene.add(gltf.scene);
-});
-
-// Render the scene
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
