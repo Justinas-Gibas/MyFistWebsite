cbdb90ec09a1deafb9937de89cfd2d531d86ea5d
JustGibas <justas.gibas@gmail.com>
2023-05-16 04:41:05 +0300

    good up

--- deeper/L2Web/scripts/slideshow.js
+++ deeper/L2Web/scripts/slideshow.js
@@ -4,6 +4,8 @@ const nextButton = document.querySelector('.controls #nextButton');
 const prevButton = document.querySelector('.controls #prevButton');
 
 let activeCard = 0;
+let touchStartX = 0;
+let touchEndX = 0;
 
 function setActiveCard(index) {
     projectCards.forEach((card, i) => {
@@ -15,34 +17,40 @@ function setActiveCard(index) {
     });
     activeCard = index;
   
-    scrollToCard(index); // Add this line to scroll to the newly activated card
+    /*scrollToCard(index);*/ // Add this line to scroll to the newly activated card
   }
 
 function scrollToCard(index) {
-    const card = projectCards[index];
-    if (card) {
-      const cardWidth = card.offsetWidth;
-      const scrollLeft = projectContainer.scrollLeft;
-      const targetLeft = card.offsetLeft - (projectContainer.offsetWidth - cardWidth) / 2;
-  
+  const card = projectCards[index];
+  if (card) {
+    const containerWidth = projectContainer.offsetWidth;
+    const containerScrollLeft = projectContainer.scrollLeft;
+    const cardOffsetLeft = card.offsetLeft;
+    const cardWidth = projectCards[index].offsetWidth;
+    const targetLeft = projectCards[index].offsetLeft - (containerWidth - cardWidth) / 2;
+    const maxScrollLeft = projectContainer.scrollWidth - containerWidth;
+    const scrollLeft = projectContainer.scrollLeft;
+
+    if (targetLeft < containerScrollLeft || targetLeft + containerWidth > containerScrollLeft + containerWidth) {
       projectContainer.scrollTo({
-        left: targetLeft + scrollLeft,
+        left: Math.min(maxScrollLeft, Math.max(0, targetLeft)),
         behavior: 'smooth',
-      });
+    });
     }
+  }
 }
 
 nextButton.addEventListener('click', () => {
   if (activeCard < projectCards.length - 1) {
     setActiveCard(activeCard + 1);
-    scrollToCard(activeCard + 1);
+    scrollToCard(activeCard);
   }
 });
 
 prevButton.addEventListener('click', () => {
   if (activeCard > 0) {
     setActiveCard(activeCard - 1);
-    scrollToCard(activeCard - 1);
+    scrollToCard(activeCard);
   }
 });
 
@@ -52,6 +60,49 @@ projectCards.forEach((card, i) => {
     scrollToCard(i);
   });
 });
+
+projectContainer.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    const delta = Math.sign(event.deltaY);
+    if (delta > 0) {
+      if (activeCard < projectCards.length - 1) {
+        setActiveCard(activeCard + 1);
+        scrollToCard(activeCard);
+      }
+    } else if (delta < 0) {
+      if (activeCard > 0) {
+        setActiveCard(activeCard - 1);
+        scrollToCard(activeCard);
+      }
+    }
+  });
+
+projectContainer.addEventListener('touchstart', (event) => {
+  touchStartX = event.touches[0].clientX;
+});
+
+projectContainer.addEventListener('touchend', (event) => {
+  touchEndX = event.changedTouches[0].clientX;
+  handleSwipeGesture();
+});
+
+function handleSwipeGesture() {
+  const swipeThreshold = 10; // Adjust this value to control swipe sensitivity
+
+  if (touchEndX < touchStartX - swipeThreshold) {
+    // Swiped left
+    if (activeCard < projectCards.length - 1) {
+      setActiveCard(activeCard + 1);
+      scrollToCard(activeCard);
+    }
+  } else if (touchEndX > touchStartX + swipeThreshold) {
+    // Swiped right
+    if (activeCard > 0) {
+      setActiveCard(activeCard - 1);
+      scrollToCard(activeCard);
+    }
+  }
+}
 
 // Set initial active card
 setActiveCard(0);
