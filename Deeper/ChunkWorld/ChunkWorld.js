2d54c008e9e3d2367e4700af6cadd9c76bf253f2
JustGibas <justas.gibas@gmail.com>
2023-07-04 23:53:56 +0300

    up

--- Deeper/ChunkWorld/ChunkWorld.js
+++ Deeper/ChunkWorld/ChunkWorld.js
@@ -127,7 +127,7 @@ function loadModel(chunk, modelPath) {
 }
 
 // Chunk settings
-const CHUNK_DISTANCE = 10; // Number of chunks in each direction to load
+const CHUNK_DISTANCE = 3; // Number of chunks in each direction to load
 
   function updateChunks(character) {
     const currentChunk = getCurrentChunk(character);
@@ -136,7 +136,7 @@ const CHUNK_DISTANCE = 10; // Number of chunks in each direction to load
       return;
     }
     lastChunkPosition = { ...currentChunk };
-      console.log("Chunk lastChunkPosition changed", Chunk);
+      console.log("Chunk lastChunkPosition changed");
   
     for (let x = currentChunk.x - CHUNK_DISTANCE; x <= currentChunk.x + CHUNK_DISTANCE; x++) {
       for (let y = currentChunk.y - CHUNK_DISTANCE; y <= currentChunk.y + CHUNK_DISTANCE; y++) {
