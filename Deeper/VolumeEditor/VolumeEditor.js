e2ad056cf3f3d7f16b1fe4033b3c9b40b697f206
JustGibas <justas.gibas@gmail.com>
2023-07-05 13:26:42 +0300

    f

--- Deeper/VolumeEditor/VolumeEditor.js
+++ Deeper/VolumeEditor/VolumeEditor.js
@@ -7,52 +7,41 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.7);
 document.body.appendChild(renderer.domElement);
 
 // Add OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Create a grid of cubes
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5); // Make the cubes smaller
-const material = new THREE.MeshBasicMaterial({color: 0x808080}); // Make the cubes gray
-const cubes = [];
+// Define the size of the grid
+const gridSize = 10;
 
-for(let i = -5; i <= 5; i++) {
-    for(let j = -5; j <= 5; j++) {
-        for(let k = -5; k <= 5; k++) {
-            const cube = new THREE.Mesh(geometry, material);
-            cube.position.set(i * 2, j * 2, k * 2);
-            scene.add(cube);
-            cubes.push(cube);
-        }
+// Create a 3D array to represent the grid
+let grid = new Array(gridSize).fill().map(() => 
+    new Array(gridSize).fill().map(() => 
+        new Array(gridSize).fill(0)
+    )
+);
+
+// Define a block
+class Block {
+    constructor(x, y, z, type) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.type = type; // This could be a number representing different block types
     }
 }
 
-// Set the camera position
-camera.position.z = 5;
+// Function to add a block to the grid
+function addBlock(x, y, z, type) {
+    let block = new Block(x, y, z, type);
+    grid[x][y][z] = block;
+}
 
-// Add a raycaster and a mouse vector
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
+// Add a block to the grid
+addBlock(1, 1, 1, 1);
 
-// Add an event listener for when the user clicks
-window.addEventListener('click', (event) => {
-    // Calculate where the user clicked in normalized device coordinates
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    // Update the picking ray with the camera and mouse position
-    raycaster.setFromCamera(mouse, camera);
-
-    // Calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects(cubes);
-
-    // Change the color of the intersected objects
-    for(let i = 0; i < intersects.length; i++) {
-        intersects[i].object.material.color.set(0xff0000);
-    }
-});
 
 // Render the scene
 function animate() {
